1c6f2fc685a70918b4524bdcb57b02146ee748d5
kobe73er <262103206@qq.com>
2016-06-13 20:09:38 +0800

    Initial Commit

---
+++ TableViewTutorail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewTutorail
+//
+//  Created by kobe73er on 16/6/13.
+//  Copyright © 2016年 kobe73er. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
